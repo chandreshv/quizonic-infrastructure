f8d481d0e2ec399b44c7f0c5dce91625bc977e8b
Sonika Mishra <sonika.vashistha86@gmail.com>
2025-02-15 17:19:24 +0530

    Initial commit - Quizonic Infrastructure

--- src/main/java/com/myorg/QuizonicCdkStack.java
+++ src/main/java/com/myorg/QuizonicCdkStack.java
@@ -3,10 +3,14 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ec2.*;
+
+import java.util.List;
+
 
 public class QuizonicCdkStack extends Stack {
+
+    private final Vpc vpc;
     public QuizonicCdkStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -14,11 +18,25 @@ public class QuizonicCdkStack extends Stack {
     public QuizonicCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        this.vpc = Vpc.Builder.create(this, "QuizonicVPC")
+                .maxAzs(2)
+                .natGateways(1)
+                .subnetConfiguration(List.of(
+                        SubnetConfiguration.builder()
+                                .cidrMask(24)
+                                .name("PublicSubnet")
+                                .subnetType(SubnetType.PUBLIC)
+                                .build(),
+                        SubnetConfiguration.builder()
+                                .cidrMask(24)
+                                .name("PrivateSubnet")
+                                .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
+                                .build()
+                ))
+                .build();
+    }
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "QuizonicCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+    public Vpc getVpc() {
+        return vpc;
     }
 }
