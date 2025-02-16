0f331df9ceca0e566daf64aa6aa71be12b653377
Sonika Mishra <sonika.vashistha86@gmail.com>
2025-02-16 15:50:26 +0530

    Added and applied spotless plugin to keep the code formatting consistent in infrastructure project

--- src/main/java/com/myorg/QuizonicCdkApp.java
+++ src/main/java/com/myorg/QuizonicCdkApp.java
@@ -1,16 +1,11 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
-
-import java.util.Arrays;
 
 public class QuizonicCdkApp {
-    public static void main(final String[] args) {
-        App app = new App();
-        new QuizonicCdkStack(app, "QuizonicCdkStack");
-        app.synth();
-    }
+  public static void main(final String[] args) {
+    App app = new App();
+    new QuizonicCdkStack(app, "QuizonicCdkStack");
+    app.synth();
+  }
 }
-
--- src/main/java/com/myorg/QuizonicCdkStack.java
+++ src/main/java/com/myorg/QuizonicCdkStack.java
@@ -1,42 +1,42 @@
 package com.myorg;
 
-import software.constructs.Construct;
+import java.util.List;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.*;
-
-import java.util.List;
-
+import software.constructs.Construct;
 
 public class QuizonicCdkStack extends Stack {
 
-    private final Vpc vpc;
-    public QuizonicCdkStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+  private final Vpc vpc;
 
-    public QuizonicCdkStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+  public QuizonicCdkStack(final Construct scope, final String id) {
+    this(scope, id, null);
+  }
 
-        this.vpc = Vpc.Builder.create(this, "QuizonicVPC")
-                .maxAzs(2)
-                .natGateways(1)
-                .subnetConfiguration(List.of(
-                        SubnetConfiguration.builder()
-                                .cidrMask(24)
-                                .name("PublicSubnet")
-                                .subnetType(SubnetType.PUBLIC)
-                                .build(),
-                        SubnetConfiguration.builder()
-                                .cidrMask(24)
-                                .name("PrivateSubnet")
-                                .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
-                                .build()
-                ))
-                .build();
-    }
+  public QuizonicCdkStack(final Construct scope, final String id, final StackProps props) {
+    super(scope, id, props);
 
-    public Vpc getVpc() {
-        return vpc;
-    }
+    this.vpc =
+        Vpc.Builder.create(this, "QuizonicVPC")
+            .maxAzs(2)
+            .natGateways(1)
+            .subnetConfiguration(
+                List.of(
+                    SubnetConfiguration.builder()
+                        .cidrMask(24)
+                        .name("PublicSubnet")
+                        .subnetType(SubnetType.PUBLIC)
+                        .build(),
+                    SubnetConfiguration.builder()
+                        .cidrMask(24)
+                        .name("PrivateSubnet")
+                        .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
+                        .build()))
+            .build();
+  }
+
+  public Vpc getVpc() {
+    return vpc;
+  }
 }
