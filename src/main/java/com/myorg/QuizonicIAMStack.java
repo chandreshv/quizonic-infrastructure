ecafd4c9954180d3d279b7bcab9aa64ffb6a742b
Chandresh Vashistha <mail2chandreshv@gmail.com>
2025-02-19 06:42:23 +0530

    Created CDK IAM Stack to create CDKDeployer role. This will enable user to assume role using sts and deploy

---
+++ src/main/java/com/myorg/QuizonicIAMStack.java
@@ -0,0 +1,51 @@
+package com.myorg;
+
+import software.amazon.awscdk.Aws;
+import software.amazon.awscdk.CfnParameter;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.iam.*;
+import software.constructs.Construct;
+
+import java.util.List;
+
+public class QuizonicIAMStack extends Stack {
+    public QuizonicIAMStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public QuizonicIAMStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        CfnParameter arnUserParameter = CfnParameter.Builder.create(this, "UserArn")
+                .type("String")
+                .description("ARN of the user who will assume the role")
+                .build();
+
+        Role cdkDeployeRole = Role.Builder.create(this, "CDKDeployerRole")
+                .assumedBy(new ArnPrincipal(arnUserParameter.getValueAsString()))
+                .roleName("CDKDeployerRole")
+                .build();
+
+        PolicyStatement policyStatementForCDKDeployment = PolicyStatement.Builder.create()
+                .effect(Effect.ALLOW)
+                .actions(List.of(
+                        "cloudformation:*",
+                        "iam:PassRole",
+                        "s3:*",
+                        "ec2:*",
+                        "ssm:GetParameter"
+                ))
+                .resources(List.of("*"))
+                .build();
+
+        PolicyStatement policyStatementToAssumeRole = PolicyStatement.Builder.create()
+                .actions(List.of("sts:AssumeRole"))
+                .effect(Effect.ALLOW)
+                .resources(List.of("*"))
+                .build();
+
+        cdkDeployeRole.addToPolicy(policyStatementForCDKDeployment);
+        cdkDeployeRole.addToPolicy(policyStatementToAssumeRole);
+    }
+}
